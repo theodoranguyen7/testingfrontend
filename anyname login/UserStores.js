2765a4cfea763827d4dca04ff8df22c85e4547e2
theodoranguyen7 <47457752+theodoranguyen7@users.noreply.github.com>
2021-02-23 21:16:32 -0800

    this needs to be its own folder. call it stores

---
+++ anyname login/UserStores.js
@@ -0,0 +1,13 @@
+import { extendObservable } from 'mobx';
+
+class UserStore{
+    constructor(){
+        extendObservable(this,{
+            loading: false,
+            isLoggedIn: false,
+            username: ''
+        })
+    }
+}
+
+export default new UserStore();
